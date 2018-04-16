a024a286ae24fab0f48101f95770a33918eb72f4
RobertThomas Adams <radams@blue.cs.sonoma.edu>
2018-04-15 20:20:41 -0700

    Main menu fixed. Buttons working. Cavans done.

---
+++ Assets/JoyConRKey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum JoyConRKey
+{
+	None = KeyCode.None,
+
+	A = KeyCode.JoystickButton0,
+	B = KeyCode.JoystickButton2,
+	X = KeyCode.JoystickButton1,
+	Y = KeyCode.JoystickButton3,
+	Plus = KeyCode.JoystickButton9,
+	Home = KeyCode.JoystickButton12,
+
+	R = KeyCode.JoystickButton14,
+	ZR = KeyCode.JoystickButton15,
+
+	SR = KeyCode.JoystickButton5,
+	SL = KeyCode.JoystickButton4,
+
+	// Joystick Horizontal = Joystick Axis 10.
+	// Joystick Vertical = Joystick Axis 9.
+}
