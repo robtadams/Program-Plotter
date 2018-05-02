80607788eb3860eaff3a161a5e4a2b1456db6cad
Robert Adams <rob.thomas.adams@gmail.com>
2018-05-02 11:43:40 -0700

    Re-fixed Scene Transition and Removed Extra Scenes
    Hopefully I won't have to do this again. GitHub keeps refusing to keep the changes to the stray assets and the options folder.

--- Main/SceneSwapper.cs
+++ Main/SceneSwapper.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwapper : MonoBehaviour {
+public class SceneSwapper : MonoBehaviour
+{
 
-    public void Button_Onclick(string scene)
+	public void Button_Onclick(string scene)
 	{
 		SceneManager.LoadScene(scene, LoadSceneMode.Single);
-    }
+	}
 }
