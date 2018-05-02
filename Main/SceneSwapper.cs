cf6f83aceb543b093d80e089c835b27628a748cf
Robert Adams <rob.thomas.adams@gmail.com>
2018-05-02 11:15:46 -0700

    Scene Transition on Name not Number
    * Created a new script which transitions to scene with that scene's name, not where it's loaded in the scene manager
       * Set string equal to the scene you want the button to transition to
       * Script component of empty GameObject called Scene Selected
       * Scene string located under the On Click() function of each button
          * Set the text field to which scene you want that button to transition to

---
+++ Main/SceneSwapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapper : MonoBehaviour {
+
+    public void Button_Onclick(string scene)
+	{
+		SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
