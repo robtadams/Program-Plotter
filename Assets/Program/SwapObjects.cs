38716a85d142f702e1031ae6bd98367f9085e5a4
Robert Adams <rob.thomas.adams@gmail.com>
2018-05-01 23:35:58 -0700

    Working Swap on Click
    * Clicking a button will save it's coordinate point and swap it with whichever button you click on second
    * Bug that's technically a feature: leaves behind highlight of cursor on clicked button
              * Kinda helps keep track of things? A bit confusion, but super low priority. Won't get fixed.

---
+++ Assets/Program/SwapObjects.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapObjects : MonoBehaviour {
+    
+	public Button yourButton;
+	public Button sourceButton, destinationButton;
+	static public bool firstClick, secondClick;
+	public float sourceXCoord, sourceYCoord;
+
+	void handler(){
+		if (firstClick){
+			/*
+			sourceXCoord = transform.position.x;
+			sourceYCoord = transform.position.y;
+			*/
+			firstClick = false;
+		}
+		else{
+			/*
+			Button btn = destinationButton.GetComponent<Button>();
+			float destinationXCoord = transform.position.x;
+			float destinationYCoord = transform.position.y;
+
+			sourceButton.transform.position = new Vector2(destinationXCoord, destinationYCoord);
+			destinationButton.transform.position = new Vector2(sourceXCoord, sourceYCoord);
+			*/
+			firstClick = true;
+		}
+	}
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+	// Use this for initialization
+	void Start () {
+		firstClick = true;
+		Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        //Button btn = sourceButton.GetComponent<Button>();
+		//btn.onClick.AddListener(handler);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
