0ce1f61a1a4b6fb22e508154335c6e77a3ed822c
Robert Adams <rob.thomas.adams@gmail.com>
2018-05-02 13:01:56 -0700

    Join Program and Start
    Put program scene in game scene

---
+++ Scripts/Linker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linker : MonoBehaviour {
+
+	BoolButton bButton;
+	bool old_bool;
+
+	// Use this for initialization
+	void Start () {
+		GameObject swapButton = GameObject.Find("Swap Button");
+		//GameObject printSprite = GameObject.Find("BoolText");
+		bButton = swapButton.GetComponent<BoolButton>();
+		old_bool = bButton.b;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (old_bool != bButton.b){
+			old_bool = bButton.b;
+			PrintBool booltext = GameObject.Find("BoolText").GetComponent<PrintBool>();
+			booltext.printBool(old_bool);
+		}
+	}
+}
